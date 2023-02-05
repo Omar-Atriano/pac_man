6cd78d1dfaf40f59d94cd01fb69048d21062b373
Omar <Omar@LENOVO>
2023-02-04 23:44:01 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace pac_man
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pacman = new System.Windows.Forms.PictureBox();
+            this.map = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pacman
+            // 
+            this.pacman.BackColor = System.Drawing.Color.Transparent;
+            this.pacman.Location = new System.Drawing.Point(319, 168);
+            this.pacman.Name = "pacman";
+            this.pacman.Size = new System.Drawing.Size(26, 31);
+            this.pacman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pacman.TabIndex = 0;
+            this.pacman.TabStop = false;
+            // 
+            // map
+            // 
+            this.map.BackColor = System.Drawing.Color.Transparent;
+            this.map.Location = new System.Drawing.Point(208, 31);
+            this.map.Name = "map";
+            this.map.Size = new System.Drawing.Size(535, 494);
+            this.map.TabIndex = 1;
+            this.map.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(847, 566);
+            this.Controls.Add(this.pacman);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pacman;
+        private System.Windows.Forms.PictureBox map;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using pac_man.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pac_man
+{
+    public partial class Form1 : Form
+    {
+        static Graphics g;
+        bool hold, right, left, up, down = true;
+        public Form1()
+        {
+            InitializeComponent();
+            //DrawMap1();
+            Init();
+        }
+        public void Init()
+        {
+            level1();
+
+        }
+
+        public void level1()
+        {
+            DrawMap1();
+            pacman.Image = MyResource.fijo;
+        }
+
+        public void DrawMap1()
+        {
+            Bitmap bmp = new Bitmap(400, 400);
+            g = Graphics.FromImage(bmp);
+            map.Image = bmp;
+            g.Clear(Color.Black);
+
+
+            for (int x = 0; x < mapaBase.map0.GetLength(0); x++)
+            {
+                for (int y = 0; y < mapaBase.map0.GetLength(1); y++)
+                {
+
+
+                    if (mapaBase.map0[y, x] != 0)
+                    {
+                        //g.FillRectangle(new SolidBrush(Color.FromArgb(35, 35, 35)), x * 10, y * 10, 10, 10);
+                        g.FillRectangle(new SolidBrush(Color.Blue), x * 10, y * 10, 10, 10);
+                    }
+                    else
+                        g.FillRectangle(new SolidBrush(Color.Black), x * 10, y * 10, 10, 10);
+
+                    //guía panel cuadrado para código 
+                    g.DrawRectangle(Pens.Gray, x * 10, y * 10, 10, 10);
+                }
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right & hold)
+            {
+                right = true;
+                hold = false;
+                pacman.Image = MyResource.pac_man_d;
+                pacman.Left +=5;
+            }
+            else if (e.KeyData == Keys.Left & hold)
+            {
+                left = true;
+                hold = false;
+                pacman.Image = MyResource.pac_man_i;
+            }
+            else if (e.KeyData == Keys.Up & hold)
+            {
+                up = true;
+                hold = false;
+                pacman.Image = MyResource.pac_man_ar;
+            }
+            else if (e.KeyData == Keys.Down & hold)
+            {
+                down = true;
+                hold = false;
+                pacman.Image = MyResource.pac_man_ab;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right & !hold)
+            {
+                right = false;
+                hold = true;
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Left & !hold)
+            {
+                left = false;
+                hold = true;
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Up & !hold)
+            {
+                up = false;
+                hold = true;
+                pacman.Image = MyResource.fijo;
+            }
+            else if (e.KeyData == Keys.Down & !hold)
+            {
+                down = false;
+                hold = true;
+                pacman.Image = MyResource.fijo;
+            }
+        }
+    }
+}
